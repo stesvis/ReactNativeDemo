6b6709176e61c2dc419e06417b4e1a43a36d8e6c
Cristian Merli <stesvis@gmail.com>
2022-04-06 09:41:39 -0600

    Added textAlign=right

--- screens/home/HomeScreen.jsx
+++ screens/home/HomeScreen.jsx
@@ -1,9 +1,9 @@
 import { StyleSheet, View } from "react-native";
 
+import { FlatList } from "react-native";
 import React from "react";
 import Row from "../../components/Row";
 import Text from "../../components/Text";
-import { FlatList } from "react-native";
 
 const HomeScreen = (props) => {
   const ListItem = ({ item, index }) => (
@@ -17,7 +17,7 @@ const HomeScreen = (props) => {
         <Text faIcon="suitcase" iconColor="red" style={{ color: "red" }}>
           This is some very long text for demo
         </Text>
-        <Text bold style={{ color: "green" }}>
+        <Text bold style={{ color: "green", textAlign: "right" }}>
           Apr 15, 2022
         </Text>
       </Row>
@@ -27,42 +27,31 @@ const HomeScreen = (props) => {
         <Text faIcon="suitcase" iconColor="red" style={{ color: "red" }}>
           This is some very long tex...
         </Text>
-        <Text bold style={{ color: "green" }}>
+        <Text bold style={{ color: "green", textAlign: "right" }}>
           Apr 15, 2022
         </Text>
       </Row>
 
-
       <Text bold>Solution :</Text>
       <Row style={styles.row}>
         <View style={{ flex: 4.3 }}>
-          <Text faIcon="suitcase" numberOfLines={1} iconColor="red" style={{ color: "red", width: '85%' }}>
+          <Text
+            faIcon="suitcase"
+            numberOfLines={1}
+            iconColor="red"
+            style={{ color: "red", width: "85%" }}>
             This is some very long text for demo
           </Text>
         </View>
         <View style={{ flex: 1.7 }}>
-          <Text bold numberOfLines={1} style={{ color: "green" }}>
+          <Text
+            bold
+            numberOfLines={1}
+            style={{ color: "green", textAlign: "right" }}>
             Apr 15, 2022
           </Text>
         </View>
       </Row>
-
-      <FlatList
-        data={[
-          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        ]}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={ListItem}
-        style={{ width: "100%" }}
-        contentContainerStyle={{
-          justifyContent: "center",
-          alignItems: "center"
-        }}
-      />
     </View>
   );
 };
@@ -73,7 +62,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     flex: 1,
     justifyContent: "center",
-    paddingHorizontal: 40,
+    paddingHorizontal: 0,
     paddingVertical: 10,
   },
   row: {
@@ -86,7 +75,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     overflow: "hidden",
     width: "100%",
-    alignItems: 'center'
+    alignItems: "center",
   },
 });
 
